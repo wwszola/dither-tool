97b26b96c46dac9d0f0d623b7dd9e3c6777c87e6
wwszola <wwszola0@gmail.com>
2025-01-26 11:17:43 +0100

    sticking to convention

--- src/main.js
+++ src/main.js
@@ -1,243 +1,258 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 
-import { createGUI } from './ui.js';
+import { createGUI } from "./ui.js";
 
-const canvas = document.getElementById('app');
+const canvas = document.getElementById("app");
 
 const uiConfig = {
-    file: '',
-    load: loadSourceFile,
-    resultFilename: 'result.png',
-    saveResult: saveResult,
-}
+  file: "",
+  load: loadSourceFile,
+  resultFilename: "result.png",
+  saveResult: saveResult,
+};
 
 const params = {
-    pixelate: 1,
-    onPixelateChange: (v) => {
-        computeLowResTargetSize();
-        render();
-    },
-    contrast: 0.0,
-    onContrastChange: (v) => {
-        levelsAdjustPass.uniforms.contrast.value = v.value;
-        render();
-    },
-    brightness: 0.0,
-    onBrightnessChange: (v) => {
-        levelsAdjustPass.uniforms.brightness.value = v.value;
-        render();
-    },
-}
+  pixelate: 1,
+  onPixelateChange: (v) => {
+    computeLowResTargetSize();
+    render();
+  },
+  contrast: 0.0,
+  onContrastChange: (v) => {
+    levelsAdjustPass.uniforms.contrast.value = v.value;
+    render();
+  },
+  brightness: 0.0,
+  onBrightnessChange: (v) => {
+    levelsAdjustPass.uniforms.brightness.value = v.value;
+    render();
+  },
+};
 
 const ditherShader = {
-    uniforms: {
-        tDiffuse: { value: null },
-    },
-    vertexShader: null,
-    fragmentShader: null,
+  uniforms: {
+    tDiffuse: { value: null },
+  },
+  vertexShader: null,
+  fragmentShader: null,
 };
 
 const levelsAdjustShader = {
-    uniforms: {
-        tDiffuse: { value: null },
-        contrast: { value: 0.0 },
-        brightness: { value: 0.0 },
-    },
-    vertexShader: null,
-    fragmentShader: null,
+  uniforms: {
+    tDiffuse: { value: null },
+    contrast: { value: 0.0 },
+    brightness: { value: 0.0 },
+  },
+  vertexShader: null,
+  fragmentShader: null,
 };
 let levelsAdjustPass;
 
 let renderer, lowResTarget, composer, scene, camera, mesh;
 
-async function preload(){
-    try{
-        const ditherVert = await fetch('src/glsl/dither-vert.glsl');
-        ditherShader.vertexShader = await ditherVert.text();
-        const ditherFrag = await fetch('src/glsl/dither-frag.glsl');
-        ditherShader.fragmentShader = await ditherFrag.text();
-    }catch(e){
-        console.error(e);
+async function preload() {
+  try {
+    const ditherVert = await fetch("src/glsl/dither-vert.glsl");
+    ditherShader.vertexShader = await ditherVert.text();
+    const ditherFrag = await fetch("src/glsl/dither-frag.glsl");
+    ditherShader.fragmentShader = await ditherFrag.text();
+  } catch (e) {
+    console.error(e);
+  }
+  try {
+    const levelsAdjustVert = await fetch("src/glsl/levels-adjust-vert.glsl");
+    levelsAdjustShader.vertexShader = await levelsAdjustVert.text();
+    const levelsAdjustFrag = await fetch("src/glsl/levels-adjust-frag.glsl");
+    levelsAdjustShader.fragmentShader = await levelsAdjustFrag.text();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function setup() {
+  const maxCanvasSize = getCanvasSize();
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    preserveDrawingBuffer: true,
+    antialias: false,
+  });
+  renderer.setSize(maxCanvasSize.width, maxCanvasSize.height);
+
+  lowResTarget = new THREE.WebGLRenderTarget(
+    maxCanvasSize.width,
+    maxCanvasSize.height,
+    {
+      magFilter: THREE.NearestFilter,
+      minFilter: THREE.NearestFilter,
     }
-     try{
-        const levelsAdjustVert = await fetch('src/glsl/levels-adjust-vert.glsl');
-        levelsAdjustShader.vertexShader = await levelsAdjustVert.text();
-        const levelsAdjustFrag = await fetch('src/glsl/levels-adjust-frag.glsl');
-        levelsAdjustShader.fragmentShader = await levelsAdjustFrag.text();
-    }catch(e){
-        console.error(e);
-    }
+  );
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
+  camera.position.z = 1;
+
+  const geometry = new THREE.PlaneGeometry(2, 2);
+  const material = new THREE.MeshBasicMaterial({
+    map: null,
+  });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  composer = new EffectComposer(renderer, lowResTarget);
+  const renderPass = new RenderPass(scene, camera);
+  levelsAdjustPass = new ShaderPass(levelsAdjustShader);
+  const ditherPass = new ShaderPass(ditherShader);
+  const outputPass = new OutputPass();
+  composer.addPass(renderPass);
+  composer.addPass(levelsAdjustPass);
+  composer.addPass(ditherPass);
+  composer.addPass(outputPass);
 }
 
-function setup(){
-    const maxCanvasSize = getCanvasSize();
-    renderer = new THREE.WebGLRenderer({
-        canvas: canvas,
-        preserveDrawingBuffer: true,
-        antialias: false,
-    });
-    renderer.setSize(maxCanvasSize.width, maxCanvasSize.height);
-
-    lowResTarget = new THREE.WebGLRenderTarget(maxCanvasSize.width, maxCanvasSize.height,{
-        magFilter: THREE.NearestFilter,
-        minFilter: THREE.NearestFilter,
-    });
-
-    scene = new THREE.Scene();
-
-    camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000);
-    camera.position.z = 1;
-
-    const geometry = new THREE.PlaneGeometry(2, 2);
-    const material = new THREE.MeshBasicMaterial({
-        map: null,
-    });
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    composer = new EffectComposer(renderer, lowResTarget);
-    const renderPass = new RenderPass(scene, camera);
-    levelsAdjustPass = new ShaderPass(levelsAdjustShader);
-    const ditherPass = new ShaderPass(ditherShader);
-    const outputPass = new OutputPass();
-    composer.addPass(renderPass);
-    composer.addPass(levelsAdjustPass);
-    composer.addPass(ditherPass);
-    composer.addPass(outputPass);
+function render() {
+  composer.render();
 }
 
-function render(){
-    composer.render();
+function loadSourceFile(file) {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const loader = new THREE.TextureLoader();
+    loader.load(e.target.result, setSourceTexture, undefined, console.error);
+  };
+  reader.readAsDataURL(file);
 }
 
-function loadSourceFile(file){
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        const loader = new THREE.TextureLoader();
-        loader.load(e.target.result, setSourceTexture, undefined, console.error);
+function computeLowResTargetSize() {
+  if (!mesh?.material?.map) {
+    return;
+  }
+  let width = mesh.material.map.image.width;
+  width = Math.floor(width / params.pixelate);
+  let height = mesh.material.map.image.height;
+  height = Math.floor(height / params.pixelate);
+  if (lowResTarget.width !== width || lowResTarget.height !== height) {
+    // WebGLRenderTarget.setSize calls dispose on itself,
+    // thus allowing to commit size change to gpu
+    lowResTarget.setSize(width, height);
+    // EffectComposer keeps a clone of the render target as renderTarget2(readBuffer)
+    // calling EffectComposer.reset calls dispose on the readBuffer,
+    // thus properly commiting size change to the whole postprocessing pipeline
+    composer.reset(lowResTarget);
+  }
+}
+
+function setSourceTexture(texture) {
+  if (mesh.material.map) {
+    mesh.material.map.dispose();
+  }
+  mesh.material.map = texture;
+  mesh.material.needsUpdate = true;
+
+  const size = getCanvasSize();
+  renderer.setSize(size.width, size.height);
+  computeLowResTargetSize();
+
+  render();
+}
+
+function getCanvasSize() {
+  const appContainer = document.getElementById("app-container");
+  const containerSize = {
+    width: appContainer.clientWidth,
+    height: appContainer.clientHeight,
+    aspectRatio: appContainer.clientWidth / appContainer.clientHeight,
+  };
+  const currentTexture = mesh?.material?.map;
+  let width = containerSize.width;
+  let height = containerSize.height;
+  if (currentTexture) {
+    const textureSize = {
+      width: currentTexture.image.width,
+      height: currentTexture.image.height,
+      aspectRatio: currentTexture.image.width / currentTexture.image.height,
     };
-    reader.readAsDataURL(file);
+    if (containerSize.aspectRatio > textureSize.aspectRatio) {
+      let scale = containerSize.height / textureSize.height;
+      scale = Math.pow(2, Math.floor(Math.log2(scale)));
+      height = textureSize.height * scale;
+      width = height * textureSize.aspectRatio;
+    } else {
+      let scale = containerSize.width / textureSize.width;
+      scale = Math.pow(2, Math.floor(Math.log2(scale)));
+      width = textureSize.width * scale;
+      height = width / textureSize.aspectRatio;
+    }
+  }
+  return { width: Math.floor(width), height: Math.floor(height) };
 }
 
-function computeLowResTargetSize(){
-    if(!mesh?.material?.map){
-        return;
+async function saveResult() {
+  try {
+    // Data from GPU needs to be drawn to a canvas to ensure proper file encoding
+    const offscreenCanvas = new OffscreenCanvas(
+      lowResTarget.width,
+      lowResTarget.height
+    );
+    const context = offscreenCanvas.getContext("2d");
+
+    const imageData = context.createImageData(
+      lowResTarget.width,
+      lowResTarget.height
+    );
+
+    // Read pixels from gpu
+    await renderer.readRenderTargetPixelsAsync(
+      lowResTarget,
+      0,
+      0,
+      lowResTarget.width,
+      lowResTarget.height,
+      imageData.data
+    );
+
+    // GPU pixels are flipped along y axis, ImageBitmap allows reversing the flip
+    const imageBitmap = await createImageBitmap(imageData, {
+      imageOrientation: "flipY",
+    });
+
+    context.drawImage(imageBitmap, 0, 0);
+
+    imageBitmap.close();
+
+    const filename = uiConfig.resultFilename.trim();
+    let mimeType;
+    if (filename.endsWith(".png")) {
+      mimeType = "image/png";
+    } else if (filename.endsWith(".jpg") || filename.endsWith(".jpeg")) {
+      mimeType = "image/jpeg";
+    } else {
+      throw new Error(
+        "Invalid file extension. Please use .png, .jpg, or .jpeg"
+      );
     }
-    let width = mesh.material.map.image.width;
-    width = Math.floor(width/params.pixelate);
-    let height = mesh.material.map.image.height;
-    height = Math.floor(height/params.pixelate);
-    if(lowResTarget.width !== width || lowResTarget.height !== height){
-        // WebGLRenderTarget.setSize calls dispose on itself,
-        // thus allowing to commit size change to gpu
-        lowResTarget.setSize(width, height);
-        // EffectComposer keeps a clone of the render target as renderTarget2(readBuffer)
-        // calling EffectComposer.reset calls dispose on the readBuffer,
-        // thus properly commiting size change to the whole postprocessing pipeline
-        composer.reset(lowResTarget);
-    }
+
+    const blob = await offscreenCanvas.convertToBlob({ type: mimeType });
+
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-function setSourceTexture(texture){
-    if(mesh.material.map){
-        mesh.material.map.dispose();
-    }
-    mesh.material.map = texture;
-    mesh.material.needsUpdate = true;
-
-    const size = getCanvasSize();
-    renderer.setSize(size.width, size.height);
-    computeLowResTargetSize();
-
-    render();
-}
-
-function getCanvasSize(){
-    const appContainer = document.getElementById('app-container');
-    const containerSize = {
-        width: appContainer.clientWidth,
-        height: appContainer.clientHeight,
-        aspectRatio: appContainer.clientWidth / appContainer.clientHeight,
-    };
-    const currentTexture = mesh?.material?.map;
-    let width = containerSize.width;
-    let height = containerSize.height;
-    if (currentTexture) {
-        const textureSize = {
-            width: currentTexture.image.width,
-            height: currentTexture.image.height,
-            aspectRatio: currentTexture.image.width / currentTexture.image.height,
-        };
-        if (containerSize.aspectRatio > textureSize.aspectRatio) {
-            let scale = containerSize.height / textureSize.height;
-            scale = Math.pow(2, Math.floor(Math.log2(scale)));
-            height = textureSize.height * scale;
-            width = height * textureSize.aspectRatio;
-        } else {
-            let scale = containerSize.width / textureSize.width;
-            scale = Math.pow(2, Math.floor(Math.log2(scale)));
-            width = textureSize.width * scale;
-            height = width / textureSize.aspectRatio;
-        }
-    }
-    return {width: Math.floor(width), height: Math.floor(height)};
-}
-
-async function saveResult(){
-    try{
-        // Data from GPU needs to be drawn to a canvas to ensure proper file encoding
-        const offscreenCanvas = new OffscreenCanvas(lowResTarget.width, lowResTarget.height);
-        const context = offscreenCanvas.getContext('2d');
-
-        const imageData = context.createImageData(lowResTarget.width, lowResTarget.height);
-
-        // Read pixels from gpu
-        await renderer.readRenderTargetPixelsAsync(
-            lowResTarget, 
-            0, 0, 
-            lowResTarget.width, lowResTarget.height, 
-            imageData.data
-        );
-
-        // GPU pixels are flipped along y axis, ImageBitmap allows reversing the flip
-        const imageBitmap = await createImageBitmap(imageData, {imageOrientation: 'flipY'});
-
-        context.drawImage(imageBitmap, 0, 0);
-
-        imageBitmap.close();
-
-        const filename = uiConfig.resultFilename.trim();
-        let mimeType;
-        if(filename.endsWith('.png')){
-            mimeType = 'image/png';
-        }else if(filename.endsWith('.jpg') || filename.endsWith('.jpeg')){
-            mimeType = 'image/jpeg';
-        }else{
-            throw new Error('Invalid file extension. Please use .png, .jpg, or .jpeg');
-        }
-
-        const blob = await offscreenCanvas.convertToBlob({type: mimeType});
-
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = filename;
-        link.click();
-
-    }catch(e){
-        console.error(e);
-    }
-}
-
-window.addEventListener('resize', () => {
-    const size = getCanvasSize();
-    renderer.setSize(size.width, size.height);
-    render();
+window.addEventListener("resize", () => {
+  const size = getCanvasSize();
+  renderer.setSize(size.width, size.height);
+  render();
 });
 
 await preload();
--- src/ui.js
+++ src/ui.js
@@ -1,45 +1,56 @@
-import { Pane } from 'tweakpane';
-import * as TweakpaneFileImportPlugin from 'tweakpane-plugin-file-import';
+import { Pane } from "tweakpane";
+import * as TweakpaneFileImportPlugin from "tweakpane-plugin-file-import";
 
-const uiContainer = document.getElementById('ui-container');
+const uiContainer = document.getElementById("ui-container");
 
 let pane;
 
-export function createGUI(config, params){
-    pane = new Pane({
-        container: uiContainer,
+export function createGUI(config, params) {
+  pane = new Pane({
+    container: uiContainer,
+  });
+  pane.registerPlugin(TweakpaneFileImportPlugin);
+
+  pane
+    .addBinding(config, "file", {
+      view: "file-input",
+      lineCount: 3,
+      filetypes: [".jpg", ".jpeg", ".png"],
+      invalidFiletypeMessage:
+        "Invalid file type. Please upload a .jpg, .jpeg, or .png file.",
+    })
+    .on("change", (e) => {
+      if (e.value) {
+        config.load(e.value);
+      }
     });
-    pane.registerPlugin(TweakpaneFileImportPlugin);
 
-    pane.addBinding(config, 'file', {
-        view: 'file-input',
-        lineCount: 3,
-        filetypes: ['.jpg', '.jpeg', '.png'],
-        invalidFiletypeMessage: 'Invalid file type. Please upload a .jpg, .jpeg, or .png file.',
-    }).on('change', (e) => {
-        if(e.value){
-            config.load(e.value);
-        }
-    });
+  pane.addBinding(config, "resultFilename");
+  pane
+    .addButton({
+      title: "Save Result",
+    })
+    .on("click", config.saveResult);
 
-    pane.addBinding(config, 'resultFilename');
-    pane.addButton({
-        title: 'Save Result'
-    }).on('click', config.saveResult);
-
-    pane.addBinding(params, 'pixelate', {
-        min: 1,
-        max: 16,
-        step: 1
-    }).on('change', params.onPixelateChange);
-    pane.addBinding(params, 'contrast', {
-        min: -1,
-        max: 1,
-        step: 0.01
-    }).on('change', params.onContrastChange);
-    pane.addBinding(params, 'brightness', {
-        min: -1,
-        max: 1,
-        step: 0.01
-    }).on('change', params.onBrightnessChange);
+  pane
+    .addBinding(params, "pixelate", {
+      min: 1,
+      max: 16,
+      step: 1,
+    })
+    .on("change", params.onPixelateChange);
+  pane
+    .addBinding(params, "contrast", {
+      min: -1,
+      max: 1,
+      step: 0.01,
+    })
+    .on("change", params.onContrastChange);
+  pane
+    .addBinding(params, "brightness", {
+      min: -1,
+      max: 1,
+      step: 0.01,
+    })
+    .on("change", params.onBrightnessChange);
 }
